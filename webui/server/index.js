6ae9eaa5c76ea134b85b1b617149624a58c9fba0
Brandon Bakker <brandon@bdmd.com.au>
2018-01-25 08:01:39 +1000

    Working session get

--- webui/server/index.js
+++ webui/server/index.js
@@ -1,10 +1,38 @@
 const Database = require('better-sqlite3');
-const r = require('rambda');
-const fs = require('fs');
-const express = require('express')
+// const r = require('rambda');
+// const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const db = new Database('./data/sleepdata.db', {
+  fileMustExist: true
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/session/:sessionId')
+  .get((req, res) => {
+    const sessionId = Number.parseInt(req.params.sessionId);
+
+    const sleepSessionQuery = db.prepare('SELECT * FROM SLEEPEVENT WHERE SESSION=@sessionId');
+    const rows = sleepSessionQuery.all({sessionId: sessionId});
+
+    res.json({ message: {count:  rows.length}});   
+  });
+
+app.use('/', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+
